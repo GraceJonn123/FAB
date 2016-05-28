586753e791f272342cb01e1b799edc98fb2395da
GraceJonn123 <gj.gjonn@gmail.com>
2016-05-28 12:39:37 +0300

    added content in the classes

--- app/src/main/java/com/jijjy/grace/fab/model/Circle.java
+++ app/src/main/java/com/jijjy/grace/fab/model/Circle.java
@@ -4,4 +4,18 @@ package com.jijjy.grace.fab.model;
  * Created by grace on 5/28/16.
  */
 public class Circle {
+    public float x;
+    public float y;
+    public float radius;
+
+    public Circle(){
+    }
+
+    /*
+    * restores to default values*/
+    public void restore() {
+        x = 0;
+        y = 0;
+        radius = 0;
+    }
 }
